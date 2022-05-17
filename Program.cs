ea41f942198a3944e950541231b91625477f8324
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-05-17 10:38:18 -0400

    Initial code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using static Apple.CoreGraphics;
+
+namespace PInvoke;
+
+public static class Program
+{
+    private static unsafe void Main()
+    {
+        CGWindowListOption options = 0; // kCGWindowListOptionAll
+        var x = CGWindowListCreate(options, kCGNullWindowID);
+    }
+}
---
+++ header.h
@@ -0,0 +1,2 @@
+#pragma once
+#include <CoreGraphics/CoreGraphics.h>
