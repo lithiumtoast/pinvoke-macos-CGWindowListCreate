480c5e78ca1375d27e02f32ce8a2b38867d718ad
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-05-17 20:42:23 -0400

    Progress

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ public static class Program
 {
     private static unsafe void Main()
     {
-        CGWindowListOption options = 0; // kCGWindowListOptionAll
-        var x = CGWindowListCreate(options, kCGNullWindowID);
+        var windowsIdentifiers = CGWindowListCreate(kCGWindowListOptionAll, kCGNullWindowID);
+        var windowsData = CGWindowListCreateDescriptionFromArray(windowsIdentifiers);
+        
+        var windowsCount = CFArrayGetCount(windowsData);
+        for (var i = 0; i < windowsCount; i++)
+        {
+            CFDictionaryRef dictionary = CFArrayGetValueAtIndex(windowsData, i);
+            var dictionaryItemsCount = CFDictionaryGetCount(dictionary);
+            for (var j = 0; j < dictionaryItemsCount; j++)
+            {
+                // TODO: Need the key variable as shown here: https://developer.apple.com/documentation/coregraphics/quartz_window_services/required_window_list_keys?language=objc
+                // var value = CFDictionaryGetValue(dictionary, key);
+            }
+        }
     }
 }
